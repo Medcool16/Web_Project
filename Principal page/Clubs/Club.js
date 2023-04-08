e5e74568481d2d943b90003a947e144890d7bcb1
HamzaBourras <bourrashamza2004@gmail.com>
2023-04-08 14:05:34 +0000

    update 3

--- Principal page/Clubs/Club.js
+++ Principal page/Clubs/Club.js
@@ -19,102 +19,103 @@ bloc.addEventListener('click',(EVENT)=>{
       let IDbloc = clickedImage.parentNode.parentNode.getAttribute('id');      //savoir l'id d'image cliquer 
            let H1 = document.createElement('h1');
                H1.classList.add('flo-div-heading');  //add class to the heading
-           let Para = document.createElement('p');
-              Para.classList.add('flo-div-paragraph');   //add class to the paragraph
+           let Iframe = document.createElement('iframe');
+               Iframe.classList.add('iframe-club-desc');   //add class to description
            let description ;
 
       switch (IDbloc) {
         case "D1": {  description = document.getElementById('Q1');
-                      H1.textContent = description.firstElementChild.textContent;
-                      Para.textContent = description.firstElementChild.nextElementSibling.textContent;
+                        H1.textContent = description.firstElementChild.textContent;
+                        Iframe.setAttribute('src','Clubs/description group/gr1.html');
                       floatingDiv.append(H1);
-                      floatingDiv.append(Para);
+                      floatingDiv.append(Iframe);
+                      
         }break;
 
         case "D2": {  description = document.getElementById('Q2');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr2.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
         }break;
 
         case "D3": {description = document.getElementById('Q3');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr3.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D4": {description = document.getElementById('Q4');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr4.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D5": {description = document.getElementById('Q5');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr5.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D6": {description = document.getElementById('Q6');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr6.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D7": {description = document.getElementById('Q7');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr7.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D8": {description = document.getElementById('Q8');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr8.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D9": {description = document.getElementById('Q9');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr9.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D10": {description = document.getElementById('Q10');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr10.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D11": {description = document.getElementById('Q11');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr11.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
         case "D12": {description = document.getElementById('Q12');
                     H1.textContent = description.firstElementChild.textContent;
-                    Para.textContent = description.firstElementChild.nextElementSibling.textContent;
-                    floatingDiv.append(H1);
-                    floatingDiv.append(Para);
+                    Iframe.setAttribute('src','Clubs/description group/gr12.html');
+                      floatingDiv.append(H1);
+                      floatingDiv.append(Iframe);
 
         }break;
 
@@ -138,19 +139,10 @@ bloc.addEventListener('click',(EVENT)=>{
       let sign = document.createElement('a');
       sign.textContent = "registration";
       sign.classList.add('class','Inscrir');
-      sign.setAttribute('href','Formulaire/Form.html')
+      sign.setAttribute('href','Formulaire/index.html');
       floatingDiv.append(sign);
 
-      let Iframe = document.createElement('iframe');
-              Iframe.setAttribute('src','Clubs/description group/gr.html');
-              floatingDiv.append(Iframe);
-
-           Iframe.style.cssText = `
-           width:650Px;
-           height:350px;
-           border : none;
-           `   
-    
+ 
       clickedImage.parentNode.appendChild(floatingDiv);
 }
   });
